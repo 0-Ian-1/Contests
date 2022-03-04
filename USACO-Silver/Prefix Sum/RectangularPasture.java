c1b6ff7c2e23fd5274930b4f9ed01bef3155df8d
Yian Lin (Class of 2022) <221105yl@internal.salesianum.org>
2022-03-04 15:03:19 -0500

    pairs

--- USACO-Silver/Prefix Sum/RectangularPasture.java
+++ USACO-Silver/Prefix Sum/RectangularPasture.java
@@ -3,7 +3,7 @@
  * Problem 2. Rectangular Pasture
  */
 import java.util.*;
-public class RetangularPasture {
+public class RectangularPasture {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
@@ -13,16 +13,34 @@ public class RetangularPasture {
             x[i] = scan.nextInt();
             y[i] = scan.nextInt();
         }
-        int[] rankX = new int[N];
-        int[] rankY = new int[N];
+        int[] rankX = compress(x);
+        int[] rankY = compress(y);
+        int[][] sum = new int[N+1][N+1];
         for (int i = 0; i < N; i++) {
-            int minX = Integer.MAX_VALUE;
-            int minY = Integer.MAX_VALUE;
-            int X = 0;
-            int Y = 0;
-            for (int j = 0; j < N; j++) {
-                
+            sum[rankX[i]][rankY[i]]++;
+        }
+        for (int i = 0; i < N+1; i++) {
+            for (int j = 0; j < N+1; j++) {
+                if (i > 0) sum[i][j] += sum[i-1][j];
+                if (j > 0) sum[i][j] += sum[i][j-1];
+                if (i > 0 && j > 0) sum[i][j] -= sum[i-1][j-1];
             }
         }
+        for (int i = 0; i < N+1; i++) {
+            for (int j = 0; j < N+1; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static int[] compress(int[] a) {
+        int[] c = new int[a.length];
+        int[] sort = new int[a.length];
+        for (int i = 0; i < a.length; i++) sort[i] = a[i];
+        Arrays.sort(sort);
+        for (int i = 0; i < a.length; i++) {
+            c[i] = Arrays.binarySearch(sort, a[i]);
+        }
+        return c;
     }
 }
--- USACO-Silver/Sorting-Searching/Two-Pointers/Pairup.java
+++ USACO-Silver/Sorting-Searching/Two-Pointers/Pairup.java
@@ -6,14 +6,40 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 public class Pairup {
+    static Cow[] cow;
+    static int N;
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int[] count = new int[N];
-        int[] time = new int[N];
+        N = scan.nextInt();
+        cow = new Cow[N];
         for (int i = 0; i < N; i++) {
-            count[i] = scan.nextInt();
-            time[i] = scan.nextInt();
+            int c = scan.nextInt();
+            int t = scan.nextInt();
+            cow[i] = new Cow(c, t);
         }
+        Arrays.sort(cow);
+        int count = 0;
+        int l = 0;
+        int r = N-1;
+        while (l <= r) {
+            int pair = Math.min(cow[l].c, cow[r].c);
+            if (l == r) pair /= 2;
+            count = Math.max(count, cow[l].t + cow[r].t);
+            cow[l].c -= pair;
+            cow[r].c -= pair;
+            if (cow[l].c == 0) l++;
+            if (cow[r].c == 0) r--; 
+        }
+        System.out.println(count);
+    }
+}
+class Cow implements Comparable<Cow> {
+    public int c, t;
+    public Cow(int c, int t) {
+        this.c = c; 
+        this.t = t;
+    }
+    public int compareTo(Cow that) {
+        return this.t - that.t;
     }
 }
