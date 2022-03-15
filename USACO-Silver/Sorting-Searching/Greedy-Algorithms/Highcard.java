2b0d764b884c4e2173fc1c3e5ddcbc999252327a
Yian Lin (Class of 2022) <221105yl@internal.salesianum.org>
2022-03-15 14:00:26 -0500

    hicards

---
+++ USACO-Silver/Sorting-Searching/Greedy-Algorithms/Highcard.java
@@ -0,0 +1,20 @@
+/**
+ * USACO 2015 December Contest, Silver
+ * Problem 2. High Card Wins
+ */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Highcard {
+    public static void main (String args[]) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[] b = new int[N];
+        int[] e = new int[N];
+        for (int i = 0; i < N; i++) {
+            e[i] = scan.nextInt();
+        }
+        Arrays.sort(e);
+        
+    }
+}
