9c7282c910ffdda36c2a7ee532471a1343ef1cd3
Yian Lin (Class of 2022) <221105yl@internal.salesianum.org>
2022-03-16 09:15:57 -0500

    greedy

--- USACO-Silver/Sorting-Searching/Greedy-Algorithms/Highcard.java
+++ USACO-Silver/Sorting-Searching/Greedy-Algorithms/Highcard.java
@@ -3,18 +3,27 @@
  * Problem 2. High Card Wins
  */
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 public class Highcard {
     public static void main (String args[]) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int[] b = new int[N];
-        int[] e = new int[N];
+        BufferedReader br = new BufferedReader(new FileReader("highcard.in"));
+        PrintWriter pw = new PrintWriter("highcard.out");
+        int N = Integer.parseInt(br.readLine());
+        int[] index = new int[N*2];
         for (int i = 0; i < N; i++) {
-            e[i] = scan.nextInt();
+            index[Integer.parseInt(br.readLine())-1] = 1;
         }
-        Arrays.sort(e);
-        
+        int count, total;
+        count = total = 0; 
+        for (int i = N*2-1; i > -1; i--) {
+            if (index[i] == 0) count++;
+            if (count > 0 && index[i] == 1) {
+                count--;
+                total++;
+            }
+        }
+        pw.println(total);
+        pw.close();
+        br.close();
     }
 }
