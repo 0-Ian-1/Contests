2dcba5ed1197312b28ec98df179b6e04abd62aae
Yian Lin (Class of 2022) <221105yl@internal.salesianum.org>
2022-03-25 12:45:50 -0500

    binary search algorithm needs to be updated

--- USACO-Silver/Sorting-Searching/Binary-Search/AngryCows.java
+++ USACO-Silver/Sorting-Searching/Binary-Search/AngryCows.java
@@ -8,7 +8,7 @@ public class AngryCows {
     static int N, K;
     static int[] x;
     public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
@@ -17,16 +17,24 @@ public class AngryCows {
             x[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(x);
-        while ()
+        int r, lo, hi;
+        lo = r = 0; hi = x[N-1];
+        while (lo < hi) {
+            r = (lo + hi) / 2;
+            if (simulate(r)) hi = r-1;
+            else lo = r+1;
+        }
+        System.out.println(r);
     }
     static boolean simulate(int r) {
         int index = 0;
         for (int i = 0; i < K; i++) {
             int cur = x[index];
-            while (x[index] - cur < r * 2) {
+            while (index < N && x[index] - cur < r * 2) {
                 index++;
             }
+            if (index >= N) return true;
         }
-        return index >= N;
+        return false;
     }
 }
