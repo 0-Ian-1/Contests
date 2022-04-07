1a4565b30e3f1dc376f8c3b84acf2258bc9e59af
Yian Lin (Class of 2022) <221105yl@internal.salesianum.org>
2022-04-07 10:15:05 -0500

    dp

---
+++ CSES/Dynamic-Programming/Dice.java
@@ -0,0 +1,17 @@
+/**
+ * CSES Problem Set
+ * Dice Combinations
+ */
+import java.util.*;
+public class Dice {
+    static int[] c;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+    }
+    static int count(int k) {
+        if (k == 1) c[k] = 1;
+        
+    }
+}
