e8b41d7c0de80e9c3c1fd79d3d162a3783c80713
Yian Lin (Class of 2022) <221105yl@internal.salesianum.org>
2022-04-27 07:54:07 -0500

    b

--- CSES/Dynamic-Programming/Dice.java
+++ CSES/Dynamic-Programming/Dice.java
@@ -4,14 +4,26 @@
  */
 import java.util.*;
 public class Dice {
-    static int[] c;
+    static long[] c;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-
+        c = new long[n+1];
+        long res = count(n);
+        System.out.print(res);
     }
-    static int count(int k) {
-        if (k == 1) c[k] = 1;
-        
+    static long count(int k) {
+        if (c[k] != 0) return c[k];
+        else if (k <= 2) c[k] = k;
+        else {
+            long sum = 0; 
+            for (int i = Math.max(0, k-6); i < k; i++) {
+                sum += count(i) % (10E9 + 7);
+                sum %= (10E9 + 7);
+            }
+            if (k <= 6) sum++;
+            c[k] = sum;
+        }
+        return c[k];
     }
 }
